361f4528003484e749b9caf01686bfc26c8699d2
Eli Mullan <EliMullan21@gmail.com>
2023-10-04 17:58:45 +0100

    setup routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,31 @@
+import {Outlet, Navigate, Route, Routes, useLocation} from 'react-router-dom';
+
+//When user logs in will have access to their protected route 
+function Layout() {
+  const user = null;
+  const location = useLocation();
+
+  return user?.token ? (
+    <Outlet />
+  ) : (
+    <Navigate to='/login' state={{from: location}} replace />
+  );
+}
+
 function App() {
   return (
     <div className="w-full min-h-[100vh]">
-    
+       <Routes>
+         {/* protected routes */}
+         <Route element={<Layout />}>
+            <Route path='/' element= {<Home />}/>
+            <Route path='/profile/:id?' element={<Profile />} />         
+         </Route>
+         { /* unprotected routes */}
+         <Route path='/register' element={<Register />}/>
+         <Route path='/login' element={<Login />}/>
+         <Route path='reset-password' element={<ResetPassword />} />
+       </Routes>       
     </div>
   );
 }
