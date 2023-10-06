a0fffc5e1bc50d320b8455d61719c2a2bf84cb03
Eli Mullan <EliMullan21@gmail.com>
2023-10-06 19:50:02 +0100

    build login page right side

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { TbSocial } from "react-icons/tb";
+import { BsShare } from "react-icons/bs";
+import { ImConnection } from "react-icons/im";
+import { AiOutlineInteraction } from "react-icons/ai";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { TextInput, Loading, CustomButton } from "../components/index";
+import { BgImage } from "../assets";
 
 const Login = () => {
   const {
@@ -17,6 +21,10 @@ const Login = () => {
   const [errMsg, setErrMsg] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const dispatch = useDispatch();
+
+  const onSubmit = async(data) => {
+    
+  }
 
   return (
     <div className="bg-bgColor w-full h-[100vh] flex flex-center justify-center p-6">
@@ -35,7 +43,9 @@ const Login = () => {
             Log in to your account
           </p>
           <span className="text-sm mt-2 text-ascent-2">Welcome back</span>
-          <form className="py-8 flex flex-col gap-5 overflow-auto">
+          <form className="py-8 flex flex-col gap-5 overflow-auto"
+            onSubmit = {handleSubmit(onSubmit)}
+          >
             <TextInput
               name="email"
               placeholder="email@example.com"
@@ -90,15 +100,54 @@ const Login = () => {
             )}
           </form>
 
-          <p>Don&apos;t have an account ?</p>
-           <Link 
-          to='/register'
-          className='text-sm text-right text-blue font-semibold'
-        >Create Account</Link>
-           
+          <p className="text-ascent-2 text-sm text-center">
+            Don&apos;t have an account ?
+          </p>
+          <Link
+            to="/register"
+            className="text-sm text-right text-blue font-semibold"
+          >
+            Create Account
+          </Link>
         </div>
         {/* RIGHT */}
-        <div></div>
+        <div className="hidden w-1/2 h-full lg:flex flex-col items-center justify-center bg-blue">
+          <div className="relative w-full flex flex-col items-center justify-between">
+            <img
+              src={BgImage}
+              alt="Background"
+              className="w-48 2xl:w-64 h-48 2xl:h-64 rounded-full object-cover"
+            />
+            <div className="flex items-center gap-1 py-2 px-5 mt-3">
+              <div className="bg-white px-2 py-1 rounded-full flex items-center gap-1">
+                <span>
+                  <BsShare size="14" />
+                </span>
+                <span className="text-xs font-medium">Share</span>
+              </div>
+              <div className="bg-white px-2 py-1 rounded-full flex items-center gap-1">
+                <span>
+                  <ImConnection size="14" />
+                </span>
+                <span className="text-xs font-medium">Connect</span>
+              </div>
+              <div className="bg-white px-2 py-1 rounded-full flex items-center gap-1">
+                <span>
+                  <AiOutlineInteraction size="14" />
+                </span>
+                <span className="text-xs font-medium">Interact</span>
+              </div>
+            </div>
+          </div>
+          <div className="mt-16 text-center">
+            <p className="text-base">
+              Connect with friends in this MERN social app
+            </p>
+            <p className="text-sm text-white/80">
+              Share memories with friend and the world
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
